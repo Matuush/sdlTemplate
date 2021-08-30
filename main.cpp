79146f7cb8228fa7ff24b1326b408fc44455f647
Matuush <m4tuush@gmail.com>
2021-08-30 14:33:45 +0200

    uhhhhhhhhhh
    h

--- main.cpp
+++ main.cpp
@@ -1,39 +1,25 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
+#include <iostream>
 
 #define SCREEN_WIDTH 1280 
 #define SCREEN_HEIGHT 720
+#define WINDOW_NAME "SDL Template"
 
 int main(int argc, char** argv){
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) std::cout << "Error: SDL failed to initialize\nSDL Error: " << SDL_GetError() << "\n";
 
-    SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if(!window){
-        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+    SDL_Window *window = SDL_CreateWindow(WINDOW_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if(!window) std::cout << "Error: Failed to open window\nSDL Error: " << SDL_GetError() << "\n";
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if(!renderer){
-        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+    if(!renderer) std::cout << "Error: Failed to create renderer\nSDL Error: " << SDL_GetError() << "\n";
 
     bool running = true;
     while(running){
         SDL_Event event;
         while(SDL_PollEvent(&event)){
-            switch(event.type){
-                case SDL_QUIT:
-                    running = false;
-                    break;
+            if(event.type == SDL_QUIT) running = false;
 
-                default:
-                    break;
-            }
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
